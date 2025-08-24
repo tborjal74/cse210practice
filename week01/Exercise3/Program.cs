0977086ac89f83e9e683e4332381c4d5e3013014
tborjal74 <tborjal@byupathway.edu>
2025-08-24 23:31:39 +0800

    Exercise4

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Formats.Asn1;
+using System.Reflection.PortableExecutable;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 10);
+        int guessNumber = 0;
+        string answer = "";
+        int guessCount = 0;
+
+        do
+        {
+            Console.WriteLine("What is your guess?:");
+            guessNumber = int.Parse(Console.ReadLine());
+            guessCount++;
+
+            if (guessNumber < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {guessCount} guesses.");
+                Console.WriteLine("Woud you like to play again? (y/n)");
+                answer = Console.ReadLine();
+
+                if (answer == "n" || answer == "N")
+                {
+                    Console.WriteLine("Thank you for playing!");
+                    break;
+                }
+            }
+
+        } while (answer != "Y" || answer != "y");
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List <int> numbers = new List<int>(); 
+        int number = -1;
+
+        do
+        {
+            Console.WriteLine("Enter number:");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+            numbers.Add(number);
+            if (number == 0)
+            {
+                double sum = numbers.Sum();
+                Console.WriteLine($"The sum is: {sum}");
+                double average = numbers.Average();
+                Console.WriteLine($"The average is: {average}");
+            }
+
+        } while (number != 0);
     }
 }
